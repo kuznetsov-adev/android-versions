0058ce819b6aaae5ff295c3d5be3e550f93293b2
g.kuznetsov <g.kuznetsov@siams.com>
2021-05-25 21:30:25 +0500

    AV-1: bind data with view for android list

--- app/src/main/java/com/itmo/androidversions/MainActivity.kt
+++ app/src/main/java/com/itmo/androidversions/MainActivity.kt
@@ -2,10 +2,23 @@ package com.itmo.androidversions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.itmo.androidversions.data.DataStorage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val android = DataStorage.getVersionsList()
+        val adapter = AndroidAdapter(this, android)
+        val list = findViewById<RecyclerView>(R.id.androidList)
+        list.adapter = adapter
+
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL)
+        ContextCompat.getDrawable(this, R.color.grey)?.let { decoration.setDrawable(it) }
+        list.addItemDecoration(decoration)
     }
 }
