ba394f1db1ffca222b79860b6df552099f99ac99
g.kuznetsov <g.kuznetsov@siams.com>
2021-06-01 09:48:36 +0500

    AV-1: modify DataStorage

--- app/src/main/java/com/itmo/androidversions/data/Android.kt
+++ app/src/main/java/com/itmo/androidversions/data/Android.kt
@@ -7,5 +7,9 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Android(
     val title: String,
-    @DrawableRes val imageAndroid: Int
+    @DrawableRes val imageAndroid: Int,
+    @DrawableRes val posterAndroid: Int,
+    val overview: String,
+    val releaseDate: String,
+    val trailerUrl: String
 ) : Parcelable
--- app/src/main/java/com/itmo/androidversions/data/DataStorage.kt
+++ app/src/main/java/com/itmo/androidversions/data/DataStorage.kt
@@ -8,52 +8,185 @@ object DataStorage {
             Android(
                 "Cupcake",
                 R.drawable.cupcake,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "April 27, 2009",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Donut",
                 R.drawable.donut,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "September 15, 2009",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android(
                 "Eclair",
                 R.drawable.eclair,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "October 23, 2009",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Froyo",
                 R.drawable.froyo,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "May 20, 2010",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
+
             ),
             Android("Gingerbread",
                 R.drawable.gingerbread,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "September 23, 2008",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Honeycomb",
                 R.drawable.honeycomb,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "February 22, 2011",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Jelly Bean",
                 R.drawable.jelly,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "July 9, 2012",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Kitkat",
                 R.drawable.kitkat,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "October 231, 2013",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Lollipop",
                 R.drawable.lollipop,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "October 3, 2014",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Marshmallow",
                 R.drawable.marshmallow,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "October 5, 2015",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Nougat",
                 R.drawable.nougat,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "August 22, 2016",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Oreo",
                 R.drawable.oreo,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "August 21, 2017",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Pie",
                 R.drawable.pie,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "August 6, 2018",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Android 10",
                 R.drawable.android_ten,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "September 13, 2019",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
             Android("Android 11",
                 R.drawable.android_el,
-            ),
-            Android("Android 12",
-                R.drawable.android_t,
+                R.drawable.poster,
+                "Android Cupcake (version 1.5) is the third version of Android developed by " +
+                        "Google, a major platform release deployable to Android-powered handsets " +
+                        "starting in April 2009, that is no longer supported. The release included " +
+                        "new features for users and developers, as well as changes in the Android " +
+                        "framework API. For developers, the Android 1.5 platform was available as a " +
+                        "downloadable component for the Android SDK.",
+                "September 8, 2020",
+                "https://www.youtube.com/watch?v=leCGI9flgbE"
             ),
         )
     }
